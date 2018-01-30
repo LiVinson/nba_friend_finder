ad9d4471994e58c870777fbca2f61cc238cf0d63
Lisa Vinson <Lrv0828@gmail.com>
2018-01-30 13:12:40 -0500

    Debugged post route to api/friends to correctly send request, receive response

--- app/data/friends.js
+++ app/data/friends.js
@@ -4,50 +4,60 @@ var nbaMatches = [
     {
         name: "Lebron James",
         image: "../public/assets/images/james.png/",
-        scores: [2, 4, 5, 1, 4, 3, 3, 5, 1, 2]//
+        scores: [2, 4, 5, 1, 4, 3, 3, 5, 1, 2],//
+        info: "Some info on this player"
     }, {
         name: "Kevin Durant",
         image: "../public/assets/images/durant.png/",
-        scores: [3, 1, 1, 5, 2, 5, 1, 4, 3, 4] //29
+        scores: [3, 1, 1, 5, 2, 5, 1, 4, 3, 4], //29
+        info: "Some info on this player"
     }, {
         name: "Kyrie Irving",
         image: "../public/assets/images/irving.png/",
-        scores: [4, 2, 5, 2, 3, 5, 5, 1, 4, 5] //33
+        scores: [4, 2, 5, 2, 3, 5, 5, 1, 4, 5], //33
+        info: "Some info on this player"
     },
     {
         name: "Russell Westbrook",
         image: "../public/assets/images/irving.png/",
-        scores: [1, 2, 3, 2, 1, 4, 2, 3, 5, 2] //
+        scores: [1, 2, 3, 2, 1, 4, 2, 3, 5, 2], //
+        info: "Some info on this player"
     },
     {
         name: "Stephen Curry",
         image: "../public/assets/images/irving.png/",
-        scores: [4, 1, 2, 1, 4, 5, 2, 3, 1, 2] //
+        scores: [4, 1, 2, 1, 4, 5, 2, 3, 1, 2], 
+        info: "Some info on this player"
     },
     {
         name: "Kawhi Leonard",
         image: "../public/assets/images/irving.png/",
-        scores: [2, 5, 1, 2, 1, 2, 4, 2, 1, 3] //
+        scores: [2, 5, 1, 2, 1, 2, 4, 2, 1, 3],
+        info: "Some info on this player" 
     },
     {
         name: "Lonzo Ball",
         image: "../public/assets/images/irving.png/",
-        scores: [1, 4, 2, 3, 2, 4, 1, 2, 1, 3] //
+        scores: [1, 4, 2, 3, 2, 4, 1, 2, 1, 3],
+        info: "Some info on this player" 
     },
     {
         name: "James Harden",
         image: "../public/assets/images/irving.png/",
-        scores: [2, 5, 2, 4, 1, 2, 5, 1, 3, 3] //
+        scores: [2, 5, 2, 4, 1, 2, 5, 1, 3, 3],
+        info: "Some info on this player" 
     },
     {
         name: "Kristaps Porzingis",
         image: "../public/assets/images/irving.png/",
-        scores: [3, 1, 5, 3, 1, 2, 4, 4, 2, 1]  //
+        scores: [3, 1, 5, 3, 1, 2, 4, 4, 2, 1],
+        info: "Some info on this player"  
     },
     {
         name: "Giannis Antetokounmpo",
         image: "../public/assets/images/irving.png/",
-        scores: [1, 4, 5, 2, 3, 1, 2, 4, 1, 2] //
+        scores: [1, 4, 5, 2, 3, 1, 2, 4, 1, 2],
+        info: "Some info on this player" 
     },
 ];
 
@@ -74,9 +84,8 @@ function findMatch(userScores) {
 });
 console.log(userMatch);
 
-$.post("/api/friends", function(data) {
-
-    console.log(data)
+$.post("/api/friends", userMatch, function(data) {
+    console.log(`Response received back from post request: ${data.name}`)
 })
 
 };
