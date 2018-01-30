ed674fa1038593f68d95496d5c64289c0347c1db
Lisa Vinson <Lrv0828@gmail.com>
2018-01-29 20:49:01 -0500

    Updated friends.js to include nbaMatches arr, logic to compare user and find match, added htmlRoutes export

--- server.js
+++ server.js
@@ -5,3 +5,39 @@
 
 var express = require("express");
 var bodyParser = require("body-parser");
+var path = require("path");
+
+// Tells node that we are creating an "express" server
+var app = express();
+
+// Sets an initial port. We"ll use this later in our listener
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+// ================================================================================
+// Express.static will allow you to set a static directory for things like your
+// front end javascript, images, etc
+// ================================================================================
+app.use(express.static("public"));
+
+// ================================================================================
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+// ================================================================================
+// require("./routes/apiRoutes")(app);
+require("./app/routes/htmlRoutes") (app);
+
+// =============================================================================
+// LISTENER
+// The below code effectively "starts" our server
+// =============================================================================
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+  });
+  
