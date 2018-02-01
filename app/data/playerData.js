782c171e481106c76fb5c433c5d3caad74efdc57
Lisa Vinson <Lrv0828@gmail.com>
2018-02-01 16:45:44 -0500

    updated modal styling and text, added comments

--- app/data/playerData.js
+++ app/data/playerData.js
@@ -2,59 +2,59 @@ var nbaScores = [{
         name: "Lebron James",
         imageURL: "/assets/images/lebron_james.jpg",
         scores: [5, 4, 5, 3, 5, 1, 5, 4, 5, 2],
-        info: "Some info on this player"
+        info: "Cleveland Caveliers, Power Foward "
     }, {
         name: "Kevin Durant",
         imageURL: "/assets/images/kevin_durant.jpg",
         scores: [4, 5, 5, 5, 4, 2, 4, 2, 4, 1],
-        info: "Some info on this player"
+        info: "Golden State Warriors, Power Forward"
     }, {
         name: "Kyrie Irving",
         imageURL: "/assets/images/kyrie_irving.jpg",
         scores: [1, 1, 2, 4, 4, 3, 3, 1, 2, 1],
-        info: "Some info on this player"
+        info: "Boston Celtics, Point Guard"
     },
     {
         name: "Russell Westbrook",
         imageURL: "/assets/images/russell_westbrook.jpg",
         scores: [3, 4, 3, 1, 4, 1, 4, 5, 3, 4],
-        info: "Some info on this player"
+        info: "Oklahoma City Thunder, Point Guard"
     },
     {
         name: "Stephen Curry",
         imageURL: "/assets/images/stephen_curry.jpg",
         scores: [5, 5, 2, 2, 5, 2, 5, 1, 2, 1],
-        info: "Some info on this player"
+        info: "Golden State Warriors, Point Guard"
     },
     {
         name: "Kawhi Leonard",
         imageURL: "/assets/images/kawhi_leonard.jpg",
         scores: [1, 4, 1, 3, 4, 5, 3, 3, 1, 4],
-        info: "Some info on this player"
+        info: "San Antonio Spurs, Small Forward"
     },
     {
         name: "Lonzo Ball",
         imageURL: "/assets/images/lonzo_ball.jpg",
         scores: [1, 3, 1, 3, 2, 3, 1, 3, 4, 2],
-        info: "Some info on this player"
+        info: "LA Lakers, Point Guard"
     },
     {
         name: "James Harden",
         imageURL: "/assets/images/james_harden.jpg",
         scores: [2, 2, 3, 4, 4, 2, 5, 2, 3, 5],
-        info: "Some info on this player"
+        info: "Houston Rockets, Point Guard"
     },
     {
         name: "Anthony Davis",
         imageURL: "/assets/images/anthony_davis.jpg",
         scores: [1, 4, 3, 2, 3, 4, 3, 2, 1, 5],
-        info: "Some info on this player"
+        info: "New Orleans Pelicans, Power Forward/Center"
     },
     {
         name: "Giannis Antetokounmpo",
         imageURL: "/assets/images/giannis_antetokounmpo.jpg",
         scores: [2, 2, 2, 1, 3, 4, 4, 3, 1, 1],
-        info: "Some info on this player"
+        info: "Milwaukee Bucks, Small Forward/Guard"
     },
 ];
 
